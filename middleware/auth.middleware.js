12435eaa8e4d2a3d2c882f0bc60a0f20326097bb
Stephane Gillon <stephanegillon@hotmail.com>
2022-05-24 17:02:46 +0200

    create cookies

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,41 @@
+const jwt = require('jsonwebtoken');
+const UserModel = require('../models/user.model');
+
+module.exports.checkUser = (req, res, next) => {
+    const token = req.cookies.jwt;
+    if (token) {
+        jwt.verify(token, process.env.TOKEN_SECRET, async (err, decodedToken) => {
+            if (err) {
+                console.log('bad token');
+                res.locals.user = null;
+                res.cookie('jwt', '', {maxAge:1});
+                next();
+            } else {
+                console.log('token is ok');
+                let user = await UserModel.findById(decodedToken.id);
+                res.locals.user = user;
+                next();
+            }
+        })
+    } else {
+        console.log('no token');
+        res.locals.user = null;
+        next();
+    }
+}
+
+module.exports.requireAuth = (req, res, next) => {
+    const token = req.cookies.jwt;
+    if (token) {
+        jwt.verify(token, process.env.TOKEN_SECRET, async (err, decodedToken) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(decodedToken.id);
+                next();
+            }
+        })
+    } else {
+        console.log('no token');
+    }
+}
