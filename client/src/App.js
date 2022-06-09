d68e1a794d8976137eee862b251f609948a63761
Nick Banken <Nick.banken@gmail.com>
2022-06-09 11:52:02 +0200

    Fix uid bugs

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,38 @@
-import React, { useEffect, useState } from 'react';
-import { UidContext } from './components/AppContext';
-import Routes from './components/Routes';
-import axios from 'axios';
-import { useDispatch } from 'react-redux';
-import { getUser } from './actions/user.actions';
+import React, { useEffect, useState } from "react";
+import { UidContext } from "./components/AppContext";
+import Routes from "./components/Routes";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { getUser } from "./actions/user.actions";
 
 const App = () => {
-  const [uid,setUid] = useState(null);
-  const dispatch = useDispatch();
+    const [uid, setUid] = useState(null);
+    const dispatch = useDispatch();
 
-  useEffect(()=> {
-    const fetchToken = async() => {
-      await axios({
-        method: 'get',
-        url: `${process.env.REACT_APP_API_URL}jwtid`,
-        withCredentials: true,
-      })
-      .then((res) => {setUid(res.data)})
-      .then (() => {if (uid) dispatch(getUser(uid))})
-      .catch((err) => console.log('no token'))
-    }
-    fetchToken();
-    
-  },[uid, dispatch]);
+    useEffect(() => {
+        const fetchToken = async () => {
+            await axios({
+                method: "get",
+                url: `${process.env.REACT_APP_API_URL}jwtid`,
+                withCredentials: true,
+            })
+                .then((res) => {
+                    console.log(res);
+                    setUid(res.data);
+                })
+                .then(() => {
+                    if (uid) dispatch(getUser(uid));
+                })
+                .catch((err) => console.log("no token"));
+        };
+        fetchToken();
+    }, [dispatch]);
 
-  return (
-    <UidContext.Provider value={uid}>
-      <Routes />
-    </UidContext.Provider>
-  );
+    return (
+        <UidContext.Provider value={uid}>
+            <Routes />
+        </UidContext.Provider>
+    );
 };
 
 export default App;
--- server.js
+++ server.js
@@ -1,35 +1,38 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const userRoutes = require('./routes/user.routes');
-const postRoutes = require('./routes/post.routes');
-require ('dotenv').config({path: './config/.env'});
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const userRoutes = require("./routes/user.routes");
+const postRoutes = require("./routes/post.routes");
+require("dotenv").config({ path: "./config/.env" });
 require("./config/db");
-const {checkUser, requireAuth} = require('./middleware/auth.middleware');
+const { checkUser, requireAuth } = require("./middleware/auth.middleware");
 const app = express();
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With,content-type"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
     next();
 });
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(checkUser);
 
 //jwt
-app.get('*', checkUser);
-app.get('/jwtid', requireAuth, (req, res) => {
+// app.get("*", checkUser);
+app.get("/jwtid", requireAuth, (req, res) => {
     res.status(200).send(res.locals.user._id);
-})
-
+});
 //routes
-app.use('/api/user', userRoutes);
-app.use('/api/post', postRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/post", postRoutes);
 
 //server
 app.listen(process.env.PORT, () => {
     console.log(`listening port ${process.env.PORT}`);
-})
+});
